170855a92ffd486a372d793e63db9ee69eb66f5a
Andy Tam <47776049+andytam99@users.noreply.github.com>
2022-02-09 13:07:12 -0600

    Deleted tests (#3)

--- tests/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Index from "../pages/index";
-
-describe("Index", () => {
-	it("Matches snaptchot", () => {
-		const { container } = render(<Index />);
-		expect(container).toMatchSnapshot();
-	});
-});
