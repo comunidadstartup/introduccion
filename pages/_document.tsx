fd4f703b39292abce51f09177167f203115a9ba8
Andy Tamayo <andresjt@icloud.com>
2022-02-15 17:04:14 -0600

    Added OG Meta Tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,7 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<link
-					rel="icon"
-					type="image/svg+xml"
-					href="/favicon/favicon.svg"
-				/>
+				<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
 				<link
 					rel="icon"
 					type="image/png"
@@ -21,6 +17,15 @@ export default function Document() {
 					sizes="16x16"
 					href="/favicon/favicon-16x16.png"
 				/>
+				<meta property="og:title" content="Comunidad Startup" />
+				<meta
+					property="og:description"
+					content="Una startup para startups y por el desarrollo de las startups. Nuestro éxito es el éxito de vuestra startup"
+				/>
+				<meta property="og:image:width" content="1280" />
+				<meta property="og:image:height" content="640" />
+				<meta property="og:image" content="/media/ogimage.png" />
+				<meta property="og:type" content="website" />
 			</Head>
 			<body>
 				<Main />
